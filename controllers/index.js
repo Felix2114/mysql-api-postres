e5e3b3de397deabe5b785f423e1d5904e50f5330
Felix2114 <zS23004696@estudiantes.uv.mx>
2025-04-18 16:41:46 -0600

    add button update and delete

--- controllers/index.js
+++ controllers/index.js
@@ -24,9 +24,59 @@ const getAllPostre= async (req, res) => {
     }
 };
 
+const  deletePostre  = async (req,  res)  =>  {
+   console.log('deleting user...');
+
+   try {
+      const postre = await models.User.findOne({ where: { id: req.params.id } });
+      if (postre) {
+          console.log(postre);
+          await postre.destroy();
+      }
+      else {
+         return res.status(200).json( { "error ": req.params.id  +  " no existe"});
+      }
+      return res.status(200).json( { "deleted ": req.params.id });
+   }
+   catch  (error) {
+      return res.status(500).send ( { error: error.message  } );
+   }
+
+};
+
+
+const updatePostre  = async (req,  res)  =>  {
+   console.log('updating user...');
+
+   try {
+      const postre = await models.User.findOne({ where: { id: req.params.id } });
+      if (postre) {
+          console.log(postre);
+          postre.nombre = req.body.nombre;
+          postre.descripcion = req.body.descripcion;
+          postre.categoria = req.body.categoria;
+          postre.precio = req.body.precio;
+          postre.stock = req.body.stock;
+          await postre.save();
+      }
+      else {
+         return res.status(200).json( { "error ": req.params.id  +  " no existe"});
+      }
+
+      return res.status(200).json( { "updated ": postre });
+   }
+   catch  (error) {
+      return res.status(500).send ( { error: error.message  } );
+   }
+
+};
+
+
 
 module.exports = {
     createPostre,
-    getAllPostre
+    getAllPostre,
+    deletePostre,
+    updatePostre
 };
 
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,8 @@ router.get('/', (req, res) => res.send('Welcome'));
 
 router.post('/postres', controllers.createPostre);
 router.get('/postres', controllers.getAllPostre);
+router.delete('/postres/:id', controllers.deletePostre);
+router.put('/postres/:id', controllers.updatePostre);
 
 module.exports = router;
                          
